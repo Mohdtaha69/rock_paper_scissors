50fdfecedaf858cef8ebacb79162bb34fcac183a
MohdTaha69 <174310203+Mohdtaha69@users.noreply.github.com>
2026-06-09 21:38:07 +0530

    add getcomputer choice fucntion

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hellllllo")
+function getComputerChoice(){
+    let x = parseInt(Math.random()*100%3);
+    let c ; 
+    if(x===0){
+        c = "rock"
+    }
+    else if(x==1){
+        c = "paper"
+    }
+    else{
+        c = "scissors"
+    }
+    return c;
+    
+    
+}
+// console.log(getComputerChoice())
+
+
+
